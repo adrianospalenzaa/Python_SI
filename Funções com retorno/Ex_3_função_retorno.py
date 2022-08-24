f112a00517bfbd1c7525cd66f82ad166db6e001d
Spal3nza <adrianospalenza@hotmail.com.br>
2022-08-24 19:37:56 -0300

    atualização do exe

--- Funções com retorno/Ex_3_função_retorno.py
+++ Funções com retorno/Ex_3_função_retorno.py
@@ -19,30 +19,32 @@ mensagem – não possui retorno)
 """
 
 
+
 def leitura(msg):  # Função para ler o valor (não recebe parâmetro e retorna o valor lido)
-    valor = int(input(msg))  # Recebe o valor lido e converte para inteiro
-    return valor  # Retorna o valor lido
+    print(msg)  # Imprime a mensagem
+    return float(input())  # Recebe o valor lido e converte para float
 
 
-def distancia(tempo, velocidade):  # Função para calcular a distância (recebe como parâmetro o tempo e a velocidade e retorna a distância)
+def calculo_dis(tempo, velocidade):  # Função para calcular a distância
+    # (recebe como parâmetro o tempo e a velocidade e retorna a distância)
     return tempo * velocidade  # Retorna a distância
 
 
-def litros(distancia):  # Função para calcular a quantidade de litros (recebe como parâmetro a distância e retorna os litros)
-    return distancia / 12  # Retorna os litros
+def calculo_litros(distancia):
+    return distancia / 12
 
 
-def resultado(velocidade, tempo, distancia, litros):  # Função para apresentar o resultado (recebe como parâmetro os valores e mostrar a mensagem – não possui retorno)
-    print("Velocidade média: {} Km/h".format(velocidade))  # Imprime a velocidade média
-    print("Tempo gasto: {} horas".format(tempo))  # Imprime o tempo gasto
-    print("Distância percorrida: {} Km".format(distancia))  # Imprime a distância percorrida
-    print("Quantidade de litros utilizada: {} litros".format(litros))  # Imprime a quantidade de litros utilizada
+def resultado(t, v, d, l):  # Função para apresentar o resultado
+    # (recebe como parâmetro os valores e mostrar a mensagem – não possui retorno)
+    print(f'Tempo {t}')
+    print(f'Velocidade {v}')
+    print(f'Distancia {d}')
+    print(f'Litros {l}')
 
 
-if __name__ == '__main__':  # Se o nome do módulo for main
-    velocidade = leitura('Informe a velocidade média!\n')  # Chama a função leitura e armazena o valor lido
-    tempo = leitura('Informe o tempo gasto!\n')  # Chama a função leitura e armazena o valor lido
-    distancia = distancia(tempo, velocidade)  # Chama a função distancia passando como parâmetro o tempo e a velocidade e armazena o valor retornado
-    litros = litros(distancia)  # Chama a função litros passando como parâmetro a distância e armazena o valor retornado
-    resultado(velocidade, tempo, distancia, litros)  # Chama a função resultado passando como parâmetro os valores e mostra a mensagem
-
+if __name__ == '__main__':
+    tempo = leitura("Informe o tempo gasto na viagem:")
+    velocidade = leitura("Informe a velocidade média na viagem:")
+    distancia = calculo_dis(tempo, velocidade)
+    litros = calculo_litros(distancia)
+    resultado(tempo, velocidade, distancia, litros)
